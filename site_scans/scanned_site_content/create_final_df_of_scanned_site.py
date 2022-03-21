6dea15c1528149fa89da7ed912d2e084619a9f7c
docwerder <wingbermuehle@icloud.com>
2022-03-21 18:32:48 +0100

    first try of full_scan

--- site_scans/scanned_site_content/create_final_df_of_scanned_site.py
+++ site_scans/scanned_site_content/create_final_df_of_scanned_site.py
@@ -6,14 +6,18 @@ final_db = pd.DataFrame()
 db_tmp = pd.DataFrame()
 
 for root, dirs, files in sorted(os.walk(os.getcwd())):
-   for name in sorted(files)[11:12]:
-       complete_file_name = os.path.join(root, name)
+   for name in sorted(files):
 
+       complete_file_name = os.path.join(root, name)
+       print('complete_file_name: ', complete_file_name)
        if ".DS_Store" in complete_file_name:
            continue
        elif "original_scanned" in complete_file_name:
+           print('goin')
            continue
        elif "db_final_complete" in complete_file_name:
+           continue
+       elif ".ipynb" in complete_file_name:
            continue
        elif ".py" in complete_file_name:
            continue
@@ -23,8 +27,8 @@ for root, dirs, files in sorted(os.walk(os.getcwd())):
            db_tmp = pd.read_csv(complete_file_name, index_col=0)
            #print('db_tmp: ', db_tmp)
            final_db = pd.concat([final_db, db_tmp])
-
-
+print('final_db: ', final_db)
+print('debug2')
 final_db = final_db.reset_index()
 del final_db['index']
 # print(tabulate(final_db, headers='keys', tablefmt='psql'))
@@ -37,4 +41,4 @@ save_final_file = os.path.join(root, 'db_final_complete.csv')
 #ggg = final_db[final_db['Site'] == 'nan']['Site']
 print('cols.. ', final_db['Site'].unique())
 final_db.to_csv(save_final_file)
-# print(tabulate(final_db, headers='keys', tablefmt='psql'))
+#print(tabulate(final_db, headers='keys', tablefmt='psql'))
